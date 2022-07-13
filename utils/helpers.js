c3cf7e33490871bbe3b40390454f8f3c471fcacd
TahirMurtaza <abdulessa93@gmail.com>
2022-07-14 01:49:59 +0500

    helpers added for auth and date

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,7 +2,7 @@
 const moment = require('moment-timezone');
 
 module.exports = {
-  format_date: date => {
-    return moment(date).tz('UTC').format('YYYY-MM-DD');
-  }
+    format_date: date => {
+        return moment(date).tz('UTC').format('Do MMMM YY');
+    }
 }
